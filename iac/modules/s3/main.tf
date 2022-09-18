abf1fd9d65860efb06c57208e4c6ea65576b2f1a
Pete Russell <pinkbatz@gmail.com>
2022-09-18 22:29:48 +1200

    Use S3 website config resource instead of deprecated website attribute.

--- iac/modules/s3/main.tf
+++ iac/modules/s3/main.tf
@@ -1,11 +1,6 @@
 resource "aws_s3_bucket" "gs_web" {
   bucket = "groundschool.co.nz"
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
   website_domain = "${var.site_name}"
 
   tags = {
@@ -16,4 +11,16 @@ resource "aws_s3_bucket" "gs_web" {
 resource "aws_s3_bucket_acl" "gs_web_acl" {
   bucket = "${aws_s3_bucket.gs_web.id}"
   acl = "public_read"
+}
+
+resource "aws_s3_bucket_website_configuration" "gs_web_website_configuration" {
+  bucket = "${aws_s3_bucket.gs_web.bucket}"
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
