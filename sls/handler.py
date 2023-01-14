ed16f7d7e48426b567c63684a5df2e1e411fc522
Pete Russell <pinkbatz@gmail.com>
2023-01-14 15:00:05 +1300

    Debugging.

--- sls/handler.py
+++ sls/handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hellos(event, context):
+def hello(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
