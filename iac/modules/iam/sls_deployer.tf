c53d69e155c1405f00cbf2cf2f0fee7393a483a4
Pete Russell <pinkbatz@gmail.com>
2023-01-14 14:08:16 +1300

    Create SLS deployer user.

---
+++ iac/modules/iam/sls_deployer.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_user" "sls_deployer" {
+  name = "sls_deployer"
+
+  tags = {
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_iam_access_key" "sls_deployer_access_key" {
+  user = "${aws_iam_user.sls_deployer.name}"
+}
+
+resource "aws_iam_user_policy" "sls_deployer_policy" {
+  name = "sls_deployer_policy"
+  user = "${aws_iam_user.sls_deployer.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "lambda:*",
+        "execute-api:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# NB. changing this secret will write the secret value to the output of
+# terraform plan. This needs to be run locally to avoid the secret being
+# written to the GitHub workflow logs.
+# Once terraform apply has been run, the secret value can be accessed with
+# 'terraform output -json'.
+output "sls_deployer_secret" {
+  value = "${aws_iam_access_key.github_deployer_access_key.secret}"
+  sensitive = true
+}
