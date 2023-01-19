6e8f417bacf6425ed3543e846ff9f08351c80f21
Pete Russell <pinkbatz@gmail.com>
2023-01-19 23:49:45 +1300

    Add DynamoDB permissions for SLS deployer user.

--- iac/modules/iam/sls_deployer.tf
+++ iac/modules/iam/sls_deployer.tf
@@ -26,7 +26,8 @@ resource "aws_iam_user_policy" "sls_deployer_policy" {
         "cloudFormation:*",
         "s3:*",
         "iam:*",
-        "logs:*"
+        "logs:*",
+        "dynamodb:*"
       ],
       "Effect": "Allow",
       "Resource": "*"
