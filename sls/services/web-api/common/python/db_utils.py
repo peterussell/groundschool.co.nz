a14ede677c379244dae892594b0ea4a824369b70
Pete Russell <pinkbatz@gmail.com>
2023-01-19 23:04:07 +1300

    Added POST question. Added DynamoDB seed data for exams table.

--- sls/services/web-api/common/python/db_utils.py
+++ sls/services/web-api/common/python/db_utils.py
@@ -10,3 +10,15 @@ def query_table(table_name, index_name, query_key, query_value):
         KeyConditionExpression=Key(query_key).eq(query_value)
     )
     return res['Items']
+
+def item_exists(table_name, column_name, value):
+    # If the result exists, it will contain an 'Item' key
+    return 'Item' in get_item(table_name, column_name, value)
+
+def get_item(table_name, pk_name, pk_value):
+    table = dynamodb.Table(table_name)
+    return table.get_item(Key={pk_name: pk_value})
+
+def put_item(table_name, item):
+    table = dynamodb.Table(table_name)
+    table.put_item(Item=item)
--- sls/services/web-api/functions/questions/questions.py
+++ sls/services/web-api/functions/questions/questions.py
@@ -1,8 +1,13 @@
+import json
+import uuid
+import datetime
+
 import db_utils
 import list_utils
 import request_utils
 import response_utils
 
+exams_table_name = 'exams'
 exam_questions_table_name = 'exam-questions'
 
 def get(event, context):
@@ -15,15 +20,61 @@ def get(event, context):
 
     except Exception as e:
         return response_utils.make_500(str(e))
-    
+
 def post(event, context):
-    body = {
-        "message": "Hello from POST questions",
-        "input": event,
+    try:
+        req = json.loads(event['body'])
+
+        # Check that a matching exam exists
+        exam_id = req['examId']
+        if not db_utils.item_exists(exams_table_name, 'id', exam_id):
+            return response_utils.make_500('Exam not found with ID ' + exam_id)
+
+        # Load remaining request fields
+        question = req['question']
+        correct_answer = req['correctAnswer']
+        incorrect_answer_1 = req['incorrectAnswer1']
+        incorrect_answer_2 = req['incorrectAnswer2']
+        incorrect_answer_3 = req['incorrectAnswer3']
+        syllabus_ref = ''
+        author_name = ''
+
+        if 'syllabusReference' in req:
+            syllabus_ref = req['syllabusReference']
+
+        if 'authorName' in req:
+            author_name = req['authorName']
+
+        # Add the question
+        question_id = _add_question(exam_id, question, correct_answer,
+            incorrect_answer_1, incorrect_answer_2, incorrect_answer_3,
+            syllabus_ref, author_name)
+
+        return response_utils.make_200({
+            'success': True,
+            'message': 'Question successfully added, ID: ' + question_id
+        })
+
+    except Exception as e:
+        return response_utils.make_500(str(e))
+
+def _add_question(exam_id, question, correct_answer, incorrect_answer_1,
+    incorrect_answer_2, incorrect_answer_3, syllabus_ref, author_name):
+    question_id = str(uuid.uuid4())
+    item = {
+        'id': question_id,
+        'examId': exam_id,
+        'question': question,
+        'correctAnswer': correct_answer,
+        'incorrectAnswer1': incorrect_answer_1,
+        'incorrectAnswer2': incorrect_answer_2,
+        'incorrectAnswer3': incorrect_answer_3,
+        'syllabusReference': syllabus_ref,
+        'authorName': author_name,
+        'addedDate': str(datetime.datetime.now()),
     }
-
-    return response_utils.make_200({ 'message': 'Hello from POST questions!!' })
-
+    db_utils.put_item(exam_questions_table_name, item)
+    return question_id
 
 def _get_questions(exam_id, desired_count):
     all_questions = _get_all_questions(exam_id)
