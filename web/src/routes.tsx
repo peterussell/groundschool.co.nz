56b133318da94e4585b226164cd328c50173e4b0
Pete Russell <pinkbatz@gmail.com>
2023-01-23 23:11:05 +1300

    Disable CI env var on builds. Enable articles route.

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -13,9 +13,9 @@ const routes = (
   <Switch>
     <Route path="/" component={HomePage} exact />
     <Route path="/exams" component={ExamsPage} />
-    {/* <Route path="/articles" component={ArticlesPage} />
-    <Route path="/resources" component={ResourcesPage} /> */}
-    <Route path="/contribute" component={ContributePage} />
+    <Route path="/articles" component={ArticlesPage} />
+    {/* <Route path="/resources" component={ResourcesPage} /> */}
+    {/* <Route path="/contribute" component={ContributePage} /> */}
     <Route path="/contact" component={ContactPage} />
     <Route component={NotFoundPage} />
   </Switch>
