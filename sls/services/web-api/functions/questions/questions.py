c22f65c3a22bad90db9ca910424fdc83c705128e
Pete Russell <pinkbatz@gmail.com>
2023-01-30 21:08:49 +1300

    Fix incorrect path for GET exam questions

--- sls/services/web-api/functions/questions/questions.py
+++ sls/services/web-api/functions/questions/questions.py
@@ -3,23 +3,10 @@ import uuid
 import datetime
 
 import db_utils
-import list_utils
-import request_utils
 import response_utils
 
 exams_table_name = 'exams'
 exam_questions_table_name = 'exam-questions'
-
-def get(event, context):
-    try:
-        desired_count = int(request_utils.get_querystring_param(event, 'count'))
-        exam_id = request_utils.get_path_param(event, 'examId')
-
-        questions = _get_questions(exam_id, desired_count)
-        return response_utils.make_200({ 'results': questions })
-
-    except Exception as e:
-        return response_utils.make_500(str(e))
 
 def post(event, context):
     try:
@@ -75,10 +62,3 @@ def _add_question(exam_id, question, correct_answer, incorrect_answer_1,
     }
     db_utils.put_item(exam_questions_table_name, item)
     return question_id
-
-def _get_questions(exam_id, desired_count):
-    all_questions = _get_all_questions(exam_id)
-    return list_utils.get_random_items(all_questions, desired_count)
-  
-def _get_all_questions(exam_id):
-    return db_utils.query_table(exam_questions_table_name, 'examIdIndex', 'examId', exam_id)
