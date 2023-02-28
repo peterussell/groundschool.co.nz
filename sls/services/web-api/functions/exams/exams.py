15567aaee8d62721c7893ab27ee97b9fd8b9f9ce
Pete Russell <pinkbatz@gmail.com>
2023-03-01 10:21:05 +1300

    Updated PPL Flight Radio questions

--- sls/services/web-api/functions/exams/exams.py
+++ sls/services/web-api/functions/exams/exams.py
@@ -41,7 +41,7 @@ def _get_exams():
         "name": "Flight Radio",
         "slug": "flight-radio",
         "licenseType": "PPL",
-        "availableQuestions": 147,
+        "availableQuestions": 154,
         "aspeqName": "FRTO Flight Radio",
         "aspeqExamInfo": {
             "durationMinutes": 40,
