3f7c92dc88e5e9fefed5767cafa5d441618bea63
Pete Russell <pinkbatz@gmail.com>
2024-06-25 00:53:34 -0500

    Added custom mapper for JpaExam to Exam domain entity

--- api/src/main/java/nz/co/groundschool/api/application/services/ExamService.java
+++ api/src/main/java/nz/co/groundschool/api/application/services/ExamService.java
@@ -1,21 +1,23 @@
 package nz.co.groundschool.api.application.services;
 
+import nz.co.groundschool.api.domain.entities.AspeqExamDetails;
 import nz.co.groundschool.api.domain.entities.Exam;
+import nz.co.groundschool.api.infrastructure.entities.JpaExam;
 import nz.co.groundschool.api.infrastructure.repository.ExamRepository;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ExamService implements IExamService {
 
-    private ExamRepository examRepository;
-
-    private ModelMapper modelMapper;
+    private final ExamRepository examRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
     public ExamService(ExamRepository examRepository, ModelMapper modelMapper) {
@@ -30,7 +32,33 @@ public class ExamService implements IExamService {
                 .collect(Collectors.toList());
     }
 
-    private Exam convertToDomainEntity(nz.co.groundschool.api.infrastructure.entities.Exam repositoryExam) {
+    private Exam convertToDomainEntity(JpaExam repositoryExam) {
+        Converter<JpaExam, Exam> jpaExamConverter = new Converter<JpaExam, Exam>() {
+            public Exam convert(MappingContext<JpaExam, Exam> context) {
+                JpaExam source = context.getSource();
+                Exam destination = context.getDestination() == null ? new Exam() : context.getDestination();
+
+                // Map 1:1 fields
+                destination.setId(source.getId());
+                destination.setName(source.getName());
+                destination.setLicenseType(source.getLicenseType());
+                destination.setSlug(source.getSlug());
+                destination.setNumAvailableQuestions(source.getNumAvailableQuestions());
+
+                // Map custom aspeqDetails field
+                var aspeqDetails = new AspeqExamDetails();
+                aspeqDetails.setName(source.getAspeqName());
+                aspeqDetails.setDurationMinutes(source.getAspeqDurationMinutes());
+                aspeqDetails.setNumQuestions(source.getAspeqNumQuestions());
+
+                destination.setAspeqExamDetails(aspeqDetails);
+
+                return destination;
+            }
+        };
+
+        modelMapper.addConverter(jpaExamConverter);
+
         return modelMapper.map(repositoryExam, Exam.class);
     }
 }
