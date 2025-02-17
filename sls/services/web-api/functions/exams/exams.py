2630297409e4d0cdcdae8b6be58f5fc628f2ae20
Pete Russell <pinkbatz@gmail.com>
2025-02-17 00:15:32 -0600

    Change to force deploy

--- sls/services/web-api/functions/exams/exams.py
+++ sls/services/web-api/functions/exams/exams.py
@@ -41,7 +41,7 @@ def _get_exams():
         "name": "Flight Radio",
         "slug": "flight-radio",
         "licenseType": "PPL",
-        "availableQuestions": 159,
+        "availableQuestions": 158,
         "aspeqName": "FRTO Flight Radio",
         "aspeqExamInfo": {
             "durationMinutes": 40,
